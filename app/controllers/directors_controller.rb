b2ee9fe5dcd597dd1fcff2ecae4e015eca6e71bf
JuliaCraig03 <juliacraig@uchicago.edu>
2024-11-05 20:51:34 +0000

    Forms Working for All 3

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,4 +1,55 @@
 class DirectorsController < ApplicationController
+  def update
+    d_id = params.fetch("the_id")
+
+    matching_records = Director.where({ :id => d_id})
+    the_director = matching_records.at(0)
+
+    the_director.title = params.fetch("the_title")
+    the_director.year = params.fetch("the_year")
+    the_director.duration = params.fetch("the_duration")
+    the_director.description = params.fetch("the_description")
+    the_director.image = params.fetch("the_image")
+    the_director.id = params.fetch("the_director_id")
+
+    the_director.save
+
+    redirect_to("/directors/#{the_director.id}")
+  
+  end
+
+  def create
+    d = Director.new
+    
+    d.title = params.fetch("the_title")
+    d.year = params.fetch("the_year")
+    d.duration = params.fetch("the_duration")
+    d.description = params.fetch("the_description")
+    d.image = params.fetch("the_image")
+    d.director_id = params.fetch("the_director_id")
+
+    d.save
+
+    redirect_to("/directors")
+  end 
+
+
+  def destroy
+    the_id = params.fetch("an_id")
+
+    matching_records = Movie.where({:id => the_id})
+
+    the_movie = matching_records.at(0)
+
+    the_movie.destroy
+
+    redirect_to("/movies")
+  end 
+  
+
+
+#PREVIOUSSTUFF WHICH WAS HERE
+  
   def index
     matching_directors = Director.all
     @list_of_directors = matching_directors.order({ :created_at => :desc })
